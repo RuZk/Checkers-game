455cfe17c09d48125189953581755af33e6954bf
AndreyMlashkin <malashkin.andrey@mail.ru>
2015-01-19 22:26:27 +0300

    command applied

--- Checkers/command.cpp
+++ Checkers/command.cpp
@@ -1,5 +1,8 @@
 #include "command.h"
 
-Command::Command()
-{
-}
+Command::Command(int _fromX, int _fromY, int _toX, int _toY)
+    : m_fromX(_fromX),
+      m_fromY(_fromY),
+      m_toX(_toX),
+      m_toY(_toY)
+{}
--- Checkers/command.h
+++ Checkers/command.h
@@ -4,7 +4,15 @@
 class Command
 {
 public:
-    Command();
+    Command(int _fromX, int _fromY, int _toX, int _toY);
+    inline int fromX() const {return m_fromX;}
+    inline int fromY() const {return m_fromY;}
+    inline int toX()   const {return m_toX;}
+    inline int toY()   const {return m_toY;}
+
+private:
+    int m_fromX, m_fromY;
+    int m_toX, m_toY;
 };
 
 #endif // COMMAND_H
